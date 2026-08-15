770ed061da6f0b53c51887eaa692c4c31b466beb
Rafael Marques <marquestec@gmail.com>
2026-08-15 14:53:57 -0300

    MAP2

--- src/streams/CriandoStreams.java
+++ src/streams/CriandoStreams.java
@@ -26,8 +26,7 @@ public class CriandoStreams {
 		outrasLangs.parallelStream().forEach(print);
 		
 		//Stream.generate(() -> "a").forEach(print);
-		
-		Stream.iterate(0, n -> n + 1).forEach(println);
+		//Stream.iterate(0, n -> n + 1).forEach(println);
 	}
 	
 
