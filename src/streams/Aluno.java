84b0ab9f90fbdd499ed7f1b1833ab5cb40ca5472
Rafael <marquestec@gmail.com>
2026-09-24 13:09:21 -0300

    Outros

--- src/streams/Aluno.java
+++ src/streams/Aluno.java
@@ -23,4 +23,39 @@ public class Aluno {
 	public String toString() {
 		return nome + " Tem nota: " + nota;
 }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (bomComportamento ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		if (bomComportamento != other.bomComportamento)
+			return false;
+		return true;
+	}
+
+
 }
